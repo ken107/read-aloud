ceec718e585d81098a66f38f540a190eb47713cc
Hai Phan <hai.phan@gmail.com>
2016-10-13 14:12:54 -0700

    updates

--- js/content.js
+++ js/content.js
@@ -1,2 +1,28 @@
 
-'Speaking rate relative to the default rate for this voice.'
+(function() {
+  var current;
+  next();
+
+  function next() {
+    current = findNext(current);
+    if (current) {
+      chrome.runtime.sendMessage({ method: "speak", text: $(current).text() }, function(response) {
+        if (response.method == "onComplete") next();
+      });
+    }
+  }
+})();
+
+function findNext(current) {
+  var node = current ? nextNode(current, true) : nextNode(document.body);
+  while (node) {
+    if (["H1","H2","H3","H4","H5","H6","P"].indexOf(node.tagName) != -1) return node;
+    node = nextNode(node);
+  }
+}
+
+function nextNode(node, skipChildren) {
+  if (node.nodeType == 1 && $(node).is(":visible") && !skipChildren && node.firstChild) return node.firstChild;
+  if (node.nextSibling) return node.nextSibling;
+  return nextNode(node.parentNode, true);
+}
--- js/events.js
+++ js/events.js
@@ -1,12 +1,24 @@
 
 chrome.browserAction.onClicked.addListener(function() {
   chrome.tabs.executeScript({ file: "js/jquery-3.1.1.min.js" }, function() {
-    chrome.tabs.executeScript({ file: "js/content.js"}, function(content) {
-      chrome.storage.sync.get("voiceName", function(saved) {
-        chrome.tts.speak(content[0], {
-          voiceName: saved.voiceName || "Google US English"
-        });
-      });
-    })
+    chrome.tabs.executeScript({ file: "js/content.js"});
   });
 });
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.method == "speak") {
+    speak(request, sendResponse);
+    return true;
+  }
+});
+
+function speak(request, sendResponse) {
+  chrome.storage.sync.get("voiceName", function(saved) {
+    chrome.tts.speak(request.text, {
+      voiceName: saved.voiceName || "Google US English",
+      onEvent: function(event) {
+        if (event.type == "end") sendResponse({ method: "onComplete" });
+      }
+    });
+  });
+}
