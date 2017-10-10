0cb9c54d49a75067d2ebd490a246604679bac86f
Hai Phan <hai.phan@gmail.com>
2017-10-10 15:31:30 -0700

    new logic

--- js/content.js
+++ js/content.js
@@ -1,4 +1,8 @@
 
+var readAloud = {
+  paraSplitter: /(?:\s*\r?\n\s*){2,}/
+};
+
 function connect(name) {
   if (!window.docReady) window.docReady = makeDoc();
   window.docReady.then(function(doc) {startService(name, doc)});
@@ -42,7 +46,7 @@ function startService(name, doc) {
 
   function getTexts(request) {
     if (request.index < 0) {
-      if (request.index == -100) return getSelectedText().split(/(?:\r?\n){2,}/);
+      if (request.index == -100) return getSelectedText().split(readAloud.paraSplitter);
       else return null;
     }
     else {
@@ -306,7 +310,7 @@ function QuoraPage() {
     $(".AnswerBase")
       .each(function() {
         texts.push("Answer by " + $(this).find(".feed_item_answer_user .user").get(0).innerText);
-        texts.push.apply(texts, $(this).find(".rendered_qtext").get(0).innerText.split(/\n\n+/));
+        texts.push.apply(texts, $(this).find(".rendered_qtext").get(0).innerText.split(readAloud.paraSplitter));
         texts.push($(this).find(".AnswerFooter").get(0).innerText);
       })
     return texts;
@@ -338,9 +342,7 @@ function KhanAcademy() {
 
 
 function HtmlDoc() {
-  var headingTags = ["H1", "H2", "H3", "H4", "H5", "H6"];
-  var paragraphTags = ["P", "BLOCKQUOTE", "PRE"];
-  var listTags = ["OL", "UL"];
+  var headingTags = "H1, H2, H3, H4, H5, H6";
 
   this.getCurrentIndex = function() {
     return 0;
@@ -355,55 +357,84 @@ function HtmlDoc() {
     //clear markers
     $(".read-aloud").removeClass("read-aloud");
 
-    //find text blocks with at least 1 paragraphs
-    var textBlocks = $("p").not("blockquote > p").parent().get();
-    $.uniqueSort(textBlocks);
-
-    //visible only
+    //find blocks containing text
+    var textBlocks = [];
+    var walk = function() {
+      if (isTextBlock(this)) textBlocks.push(this);
+      else $(this).children().each(walk);
+    };
+    walk.call(document.body);
     textBlocks = $(textBlocks).filter(":visible").filter(notOutOfView).get();
 
     if (textBlocks.length) {
       //remove any block less than 1/7 the length of the longest block
       var lengths = textBlocks.map(function(block) {
-        return $(block).children(paragraphTags.join(", ")).text().length;
+        return block.innerText.length;
       });
-      var longest = Math.max.apply(null, lengths);
+      var threshold = Math.max.apply(null, lengths) /10;
       textBlocks = textBlocks.filter(function(block, index) {
-        return lengths[index] > longest/7;
+        return lengths[index] > threshold;
       });
 
       //mark the elements to be read
       textBlocks.forEach(function(block) {
         $(findHeadingsFor(block)).addClass("read-aloud");
-        $(block).children(headingTags.concat(paragraphTags).join(", ")).addClass("read-aloud");
-        $(block).children(listTags.join(", ")).children("li").addClass("read-aloud");
+        $(block).addClass("read-aloud");
       });
     }
     else {
       //if no text blocks found, read all headings
-      $(headingTags.concat(paragraphTags).join(", ")).filter(":visible").addClass("read-aloud");
+      $(headingTags).filter(":visible").addClass("read-aloud");
     }
 
     //extract texts
-    var texts = $(".read-aloud").get().map(getText).filter(isNotEmpty);
-    return texts;
+    var texts = $(".read-aloud").get().map(getText);
+    return flatten(texts).filter(isNotEmpty);
+  }
+
+  function isTextBlock(elem) {
+    return childNodes(elem).some(function(child) {
+      return isNonEmptyTextNode(child) || $(child).is("p");
+    })
+  }
+
+  function isNonEmptyTextNode(node) {
+    return node.nodeType == 3 && node.nodeValue.trim();
   }
 
   function getText(elem) {
-    $(elem).find("sup").hide();
-    $(elem).find("*").filter(function() {return this.style && this.style.position == 'absolute'}).hide();
-    var text = elem.innerText.trim();
-    if (elem.tagName == "LI") return ($(elem).index() + 1) + ". " + text;
-    else return text;
+    $(elem).find("ol, ul").each(function() {
+      $(this).children("li").each(function(index) {
+        if (!$(this.firstChild).is(".read-aloud-numbering"))
+          $("<span>").addClass("read-aloud-numbering").text((index +1) + ". ").prependTo(this);
+      })
+    });
+    $(elem).find(".read-aloud-numbering").show();
+    var tmp = $(elem).find(":visible").filter(dontRead).toggle();
+    var texts = addMissingPunctuation(elem.innerText).trim().split(readAloud.paraSplitter);
+    tmp.toggle();
+    $(elem).find(".read-aloud-numbering").hide();
+    return texts;
+  }
+
+  function dontRead() {
+    var tagName = this.tagName;
+    var float = $(this).css("float");
+    var position = $(this).css("position");
+    return tagName == "SUP" || float == "right" || position == "absolute" || position == "fixed";
+  }
+
+  function addMissingPunctuation(text) {
+    return text.replace(/(\w)(\s*?\r?\n)/g, "$1.$2");
   }
 
   function findHeadingsFor(block) {
     var result = [];
-    var firstInnerElem = $(block).children(headingTags.concat(paragraphTags).join(", ")).get(0);
+    var firstInnerElem = $(block).find(headingTags + ", p").filter(":visible").get(0);
     var currentLevel = getHeadingLevel(firstInnerElem);
-    var node = previousNode(firstInnerElem, true);
+    var node = previousNode(block, true);
     while (node && !$(node).hasClass("read-aloud")) {
-      if (node.nodeType == 1) {
+      if (node.nodeType == 1 && $(node).is(":visible")) {
         var level = getHeadingLevel(node);
         if (level < currentLevel) {
           result.push(node);
@@ -416,8 +447,8 @@ function HtmlDoc() {
   }
 
   function getHeadingLevel(elem) {
-    var index = elem ? headingTags.indexOf(elem.tagName) : -1;
-    return index == -1 ? 100 : index + 1;
+    var matches = elem && elem.tagName.match(/^H(\d)$/);
+    return matches ? Number(matches[1]) : 100;
   }
 
   function previousNode(node, skipChildren) {
@@ -429,6 +460,20 @@ function HtmlDoc() {
 
   function notOutOfView() {
     return $(this).offset().left >= 0;
+  }
+
+  function childNodes(elem) {
+    var result = [];
+    var child = elem.firstChild;
+    while (child) {
+      result.push(child);
+      child = child.nextSibling;
+    }
+    return result;
+  }
+
+  function flatten(array) {
+    return [].concat.apply([], array);
   }
 }
 
