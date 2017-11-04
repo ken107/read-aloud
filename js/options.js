629e83804f14bd8accadb049fa4a8c0d2da4bb58
Hai Phan <hai.phan@gmail.com>
2017-11-04 06:54:40 -0700

    rate edit button

--- js/options.js
+++ js/options.js
@@ -1,9 +1,15 @@
 
-$(function() {
+Promise.all([getVoices(), getSettings(), domReady()])
+  .then(spread(initialize));
+
+function initialize(voices, settings) {
+  //labels
   $("[data-i18n]").each(function() {
     var key = $(this).data("i18n");
     $(this).text(chrome.i18n.getMessage(key));
   });
+
+  //sliders
   $(".slider").each(function() {
     $(this).slider({
       min: $(this).data("min"),
@@ -11,61 +17,85 @@ $(function() {
       step: $(this).data("step")
     })
   });
+
+  //voices
+  voices.sort(voiceSorter);
+  voices.forEach(function(voice) {
+    $("<option>").val(voice.voiceName).text(voice.voiceName).appendTo($("#voices"));
+  });
+  $("#premium").toggle(!voices.some(function(voice) {return isPremiumVoice(voice.voiceName)}));
   $("#premium a").attr("href", "https://chrome.google.com/webstore/detail/premium-text-to-speech-vo/emniggnjhffhdfbiamnnjbljgapijkfk?hl=" + getUserPreferredLanguage());
+
+  //rate
+  $("#rate-edit-button").click(function() {
+    $("#rate, #rate-input-div").toggle();
+  });
+  $("#rate").on("slidechange", function() {
+    $("#rate-input").val(Math.pow($(this).data("pow"), $(this).slider("value")).toFixed(3));
+  });
+  $("#rate-input").change(function() {
+    var val = $(this).val().trim();
+    if (isNaN(val)) $(this).val(1);
+    else if (val < .1) $(this).val(.1);
+    else if (val > 10) $(this).val(10);
+    else $("#rate-edit-button").hide();
+  });
+
+  //buttons
   $("#save").click(function() {
     updateSettings({
       voiceName: $("#voices").val(),
-      rate: Math.pow($("#rate").data("pow"), $("#rate").slider("value")),
+      rate: Number($("#rate-input").val()),
       pitch: $("#pitch").slider("value"),
       volume: $("#volume").slider("value"),
       showHighlighting: Number($("[name=highlighting]:checked").val()),
     })
     .then(function() {
+      $("#save").prop("disabled", true);
       $(".status.success").show().delay(3000).fadeOut();
     });
   });
   $("#reset").click(function() {
     clearSettings().then(() => location.reload());
   });
-  getSettings().then(init);
-});
 
+  //dirty
+  $("#voices, input[name=highlighting]").change(setDirty);
+  $("#rate, #pitch, #volume").on("slidechange", setDirty);
 
-function init(settings) {
-  getVoices()
-    .then(initVoices)
-    .then(function() {
-      if (settings.voiceName) {
-        $("#voices").val(settings.voiceName);
-      }
-    })
+  //set state
+  $("#voices").val(settings.voiceName || "");
   $("#rate").slider("value", Math.log(settings.rate || defaults.rate) / Math.log($("#rate").data("pow")));
+  $("#rate-input").val(settings.rate || defaults.rate);
   $("#pitch").slider("value", settings.pitch || defaults.pitch);
   $("#volume").slider("value", settings.volume || defaults.volume);
   $("[name=highlighting]").prop("checked", false);
   $("[name=highlighting][value=" + (settings.showHighlighting != null ? settings.showHighlighting : defaults.showHighlighting) + "]").prop("checked", true);
+  $("#save").prop("disabled", true);
 }
 
-function initVoices(voices) {
-  voices.sort(function(a,b) {
-    if (isRemoteVoice(a.voiceName)) {
-      if (isRemoteVoice(b.voiceName)) return a.voiceName.localeCompare(b.voiceName);
-      else return 1;
-    }
-    else {
-      if (isRemoteVoice(b.voiceName)) return -1;
-      else return a.voiceName.localeCompare(b.voiceName);
-    }
-  });
-  voices.forEach(function(voice) {
-    $("<option>")
-      .val(voice.voiceName)
-      .text(voice.voiceName)
-      .appendTo($("#voices"));
-  });
-  $("#premium").toggle(!voices.some(function(voice) {return isPremiumVoice(voice.voiceName)}));
+
+function domReady() {
+  return new Promise(function(fulfill) {
+    $(fulfill);
+  })
+}
+
+function voiceSorter(a,b) {
+  if (isRemoteVoice(a.voiceName)) {
+    if (isRemoteVoice(b.voiceName)) return a.voiceName.localeCompare(b.voiceName);
+    else return 1;
+  }
+  else {
+    if (isRemoteVoice(b.voiceName)) return -1;
+    else return a.voiceName.localeCompare(b.voiceName);
+  }
 }
 
 function getUserPreferredLanguage() {
   return navigator.languages ? navigator.languages[0] : navigator.userLanguage || navigator.language;
+}
+
+function setDirty() {
+  $("#save").prop("disabled", false);
 }
