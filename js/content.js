5b58e6175ab7888f988e1ce486595f5ae3520cad
Hai Phan <hai.phan@gmail.com>
2018-03-02 11:12:24 -0800

    v1.2.15 workaround for GDocs issue: scrolling to another page and selecting text does not clear previous selection

--- js/content.js
+++ js/content.js
@@ -106,22 +106,19 @@ function makeDoc() {
 function GoogleDoc() {
   var viewport = $(".kix-appview-editor").get(0);
   var pages = $(".kix-page");
+  var selectionState;
 
   this.ready = readAloud.loadScript(chrome.runtime.getURL("js/googleDocsUtil.js"));
 
   this.getCurrentIndex = function() {
-    var doc = googleDocsUtil.getGoogleDocument();
-    if (doc.selectedText) return 9999;
+    if (getSelectedText()) return 9999;
 
     for (var i=0; i<pages.length; i++) if (pages.eq(i).position().top > viewport.scrollTop+$(viewport).height()/2) break;
     return i-1;
   }
 
   this.getTexts = function(index, quietly) {
-    if (index == 9999) {
-      var doc = googleDocsUtil.getGoogleDocument();
-      return [doc.selectedText];
-    }
+    if (index == 9999) return [getSelectedText()];
 
     var page = pages.get(index);
     if (page) {
@@ -140,6 +137,25 @@ function GoogleDoc() {
     return $(".kix-paragraphrenderer", this).get()
       .map(getInnerText)
       .filter(isNotEmpty);
+  }
+
+  function getSelectedText() {
+    var doc = googleDocsUtil.getGoogleDocument();
+    //first time
+    if (!selectionState) selectionState = {caret: doc.caret.index, prev: [], text: doc.selectedText};
+    //if caret has not moved, assume selection state hasn't changed
+    //if caret has moved, update selection state
+    if (selectionState.caret != doc.caret.index) {
+      selectionState.caret = doc.caret.index;
+      selectionState.prev.push(selectionState.text);
+      selectionState.text = doc.selectedText;
+      selectionState.prev = selectionState.prev.filter(function(text) {
+        var index = selectionState.text.indexOf(text);
+        if (index != -1) selectionState.text = selectionState.text.slice(0,index) + selectionState.text.slice(index+text.length);
+        return index != -1;
+      })
+    }
+    return selectionState.text;
   }
 }
 
