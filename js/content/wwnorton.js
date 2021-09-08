a40a7710965da9160694bd606ce622b3933b96e6
Hai Phan <hai.phan@gmail.com>
2021-09-08 12:21:24 -0700

    waitFrameChange for more reliable pageflipping (Google Play, Chegg, wwnorton)

--- js/content/wwnorton.js
+++ js/content/wwnorton.js
@@ -2,6 +2,7 @@
 var rad = readAloudDoc
 var prevBtn = document.getElementById("control_previous_page")
 var nextBtn = document.getElementById("control_next_page")
+var contentFrame = document.getElementById("section_iframe")
 var currentIndex = 0
 
 readAloudDoc = {
@@ -11,12 +12,14 @@ readAloudDoc = {
     getTexts: function(index) {
         var promise = Promise.resolve()
         var rewind = function() {
+            var oldUrl = contentFrame.contentDocument.location.href
             $(prevBtn).click()
-            return waitMillis(2500)
+            return waitFrameChange(oldUrl)
         }
         var forward = function() {
+            var oldUrl = contentFrame.contentDocument.location.href
             $(nextBtn).click()
-            return waitMillis(2500)
+            return waitFrameChange(oldUrl)
         }
         for (; currentIndex<index; currentIndex++) promise = promise.then(forward)
         for (; currentIndex>index; currentIndex--) promise = promise.then(rewind)
@@ -24,4 +27,18 @@ readAloudDoc = {
             return rad.getTexts(rad.getCurrentIndex())
         })
     }
+}
+
+function waitFrameChange(oldUrl) {
+    return repeat({
+        action: function() {return contentFrame.contentDocument.location.href},
+        until: function(url) {return url != oldUrl},
+        max: 20,
+        delay: 500
+    })
+    .then(function() {
+        return new Promise(function(fulfill) {
+            return $(contentFrame.contentDocument).ready(fulfill)
+        })
+    })
 }
