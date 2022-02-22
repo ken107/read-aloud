b7b2c37463789fcdb65be55115f9a3d77d97716d
Hai Phan <hai.phan@gmail.com>
2022-02-22 11:24:44 -0800

    polyfill flatMap

--- js/defaults.js
+++ js/defaults.js
@@ -728,6 +728,42 @@ if (!Array.prototype.groupBy) {
   })
 }
 
+if (!Array.prototype.flat) {
+  Object.defineProperty(Array.prototype, 'flat', {
+    configurable: true,
+    writable: true,
+    value: function () {
+      var depth =
+        typeof arguments[0] === 'undefined' ? 1 : Number(arguments[0]) || 0;
+      var result = [];
+      var forEach = result.forEach;
+
+      var flatDeep = function (arr, depth) {
+        forEach.call(arr, function (val) {
+          if (depth > 0 && Array.isArray(val)) {
+            flatDeep(val, depth - 1);
+          } else {
+            result.push(val);
+          }
+        });
+      };
+
+      flatDeep(this, depth);
+      return result;
+    },
+  });
+}
+
+if (!Array.prototype.flatMap) {
+  Object.defineProperty(Array.prototype, 'flatMap', {
+    configurable: true,
+    writable: true,
+    value: function () {
+      return Array.prototype.map.apply(this, arguments).flat(1);
+    },
+  });
+}
+
 if (!Promise.prototype.finally) {
   Object.defineProperty(Promise.prototype, 'finally', {
     value: function(callback) {
