3939ac240639b951b0d6a7f7c7a58a012781f261
Hai Phan <hai.phan@gmail.com>
2022-08-11 08:13:03 -0700

    Revert "browserDetectLanguage don't use if not result.isReliable"
    This reverts commit 130f5ca4031ab2c15f1ace5358ba5872923e5541.

--- js/document.js
+++ js/document.js
@@ -547,7 +547,7 @@ function Doc(source, onEnd) {
       brapi.i18n.detectLanguage(text, fulfill);
     })
     .then(function(result) {
-      if (result && result.isReliable != false) {
+      if (result) {
           var list = result.languages.filter(function(item) {return item.language != "und"});
           list.sort(function(a,b) {return b.percentage-a.percentage});
           return list[0] && list[0].language;
