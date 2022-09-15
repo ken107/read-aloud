f5a6786fb1618a35bd1b5288c00262bfca1a5454
Hai Phan <hai.phan@gmail.com>
2022-09-15 13:56:45 +0700

    docs

---
+++ docs/js/utils.js
@@ -0,0 +1,89 @@
+
+var config = {
+    serviceUrl: "https://support.readaloud.app"
+};
+
+queryString = {};
+if (location.search)
+    location.search.substr(1).split('&').forEach(function(token) {
+        var pair = token.split('=');
+        queryString[decodeURIComponent(pair[0])] = pair.length > 1 ? decodeURIComponent(pair[1]) : true;
+    })
+
+function validateReturnUrl(url) {
+    return /^https:\/\/\w+\.extensions\.allizom\.org\/$/.test(url)
+        || /^https:\/\/\w+\.chromiumapp\.org\/$/.test(url)
+}
+
+function ajaxGet(url) {
+    return new Promise(function(fulfill, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url, true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == XMLHttpRequest.DONE) {
+                if (xhr.status == 200) fulfill(xhr.response);
+                else reject(new Error("Failed to fetch " + url.substr(0, 100)));
+            }
+        };
+        xhr.send(null);
+    })
+}
+
+function getCookie(name) {
+    if (document.cookie.length > 0) {
+        var start = document.cookie.indexOf(name + "=");
+        if (start != -1) {
+            start += name.length + 1;
+            var end = document.cookie.indexOf(";", start);
+            if (end == -1) {
+                end = document.cookie.length;
+            }
+            return unescape(document.cookie.substring(start, end));
+        }
+    }
+    return "";
+}
+
+function setCookie(name, value, expiredays) {
+    var cookie;
+    if (expiredays == null) {
+        cookie = name + "=" + escape(value) + "; path=/";
+    }
+    else {
+        var exdate = new Date();
+        exdate.setDate(exdate.getDate() + expiredays);
+        cookie = name + "=" + escape(value) + "; path=/" + "; expires=" + exdate.toGMTString();
+    }
+    document.cookie = cookie;
+    if (getCookie(name) != value) document.cookie = cookie + "; SameSite=None; Secure";
+}
+
+function repeat(action, opt) {
+    if (!opt) opt = {};
+    return iter(0);
+
+    function iter(n) {
+        return Promise.resolve()
+            .then(action)
+            .then(function(result) {
+                if (opt.until && opt.until(result)) return result;
+                if (opt.max && n+1 >= opt.max) return result;
+                if (!opt.delay) return iter(n+1);
+                return new Promise(function(f) {setTimeout(f, opt.delay)}).then(iter.bind(null, n+1));
+            })
+    }
+}
+
+if (!Promise.prototype.finally) {
+    Object.defineProperty(Promise.prototype, 'finally', {
+        value: function(callback) {
+            var promise = this;
+            function chain() {
+                return Promise.resolve(callback()).then(function() {return promise});
+            }
+            return promise.then(chain, chain);
+        },
+        configurable: true,
+        writable: true
+    })
+}
---
+++ docs/usage/premium-voices.md
@@ -0,0 +1,12 @@
+# Using Premium Voices (Google Wavenet & Amazon Polly)
+
+To use premium voices, follow the following procedure.
+
+
+1. Click the Read Aloud icon on the [Extensions menu](https://i.imgur.com/KTqFZ3Q.png).
+2. Stop any text that may be playing.
+3. Click on the Gear icon in the Read Aloud context menu. (It may take a second or two for settings to appear)
+4. Select `Enable Custom Voices` in ["Voice" dropdown](https://i.imgur.com/nNV5cCb.png).
+5. A window should open, now the procedure is straightforward, simply create API key on your [Google Cloud](https://cloud.google.com/text-to-speech) or [AWS](https://aws.amazon.com/polly/) account with access to voices and enter the key in [opened window](https://i.imgur.com/V8MGWol.png).
+6. Back in the options page on `Step 3`, you should be able to access and use the premium voices in the ["Voice" dropdown](https://i.imgur.com/ANoCOfE.png).
+7. Enjoy the premium voice.
