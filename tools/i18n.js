7eebb89ca570cd0bed9016374a01ebde422b24bf
Hai Phan <hai.phan@gmail.com>
2023-01-23 12:05:54 -0800

    preserve command-names during i18n translation

--- tools/i18n.js
+++ tools/i18n.js
@@ -17,26 +17,52 @@ async function run() {
   const template = await fsp.readFile("_locales/en/messages.json", "utf-8")
   for (const locale of locales) {
     console.log("Locale", locale)
+    const lang = locale.replace("_", "-")
+    const batch = makeBatch(lang)
     const file = `_locales/${locale}/messages.json`
     const input = JSON.parse(await fsp.readFile(file, "utf-8"))
     const output = JSON.parse(template)
     for (const prop in output) {
       if (prop in input) output[prop] = input[prop]
       else {
-        console.log("Translating", prop)
-        output[prop] = {message: await translate(output[prop].message, locale)}
+        const tokens = output[prop].message.split(/(\{\w+\}|(?<=])\([a-z-]+\))/)
+        const oddTokens = tokens.filter((text, i) => i % 2)
+        batch.add(tokens.map((text, i) => i % 2 ? "[1]" : text).join(""))
+          .then(result => {
+            output[prop] = {
+              message: result.split("[1]")
+                .flatMap((text, i) => oddTokens[i] ? [text, oddTokens[i]] : text)
+                .join("")
+            }
+          })
       }
     }
-    await fsp.writeFile(file, JSON.stringify(output, null, 2))
+    await batch.commit()
+    await fsp.writeFile(file, JSON.stringify(output, null, 2) + "\n")
   }
 }
 
-async function translate(text, locale) {
-  const lang = locale.replace("_", "-")
+function makeBatch(lang) {
+  const batch = []
+  return {
+    add(text) {
+      return new Promise(fulfill => batch.push({text, fulfill}))
+    },
+    async commit() {
+      if (batch.length) {
+        const results = await translate(batch.map(x => x.text), lang)
+        if (results.length != batch.length) throw new Error("Something wrong")
+        batch.forEach((entry, i) => entry.fulfill(results[i]))
+      }
+    }
+  }
+}
+
+async function translate(texts, lang) {
   const [{ translations }] = await translator.translateText({
-    contents: [text],
+    contents: texts,
     targetLanguageCode: lang,
     parent: `projects/${projectId}/locations/global`
   })
-  return translations[0].translatedText
+  return translations.map(x => x.translatedText)
 }
