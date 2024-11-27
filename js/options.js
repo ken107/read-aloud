ed7a7c3fc4fe0d0f85cc98d560140cca36d895a1
Hai Phan <hai.phan@gmail.com>
2024-11-27 09:33:49 -0800

    add "Add More Languages" option in Voice dropdown for easy access

--- js/options.js
+++ js/options.js
@@ -67,6 +67,7 @@
         .change(function() {
           var voiceName = $(this).val();
           if (voiceName == "@custom") location.href = "custom-voices.html";
+          else if (voiceName == "@languages") location.href = "languages.html";
           else if (voiceName == "@premium") brapi.tabs.create({url: "premium-voices.html"});
           else if (voiceName == "@piper") bgPageInvoke("managePiperVoices").catch(console.error)
           else updateSettings({voiceName})
@@ -362,6 +363,10 @@
     var additional = $("<optgroup>")
       .attr("label", brapi.i18n.getMessage("options_voicegroup_additional"))
       .appendTo($("#voices"));
+    $("<option>")
+      .val("@languages")
+      .text(brapi.i18n.getMessage("options_add_more_languages"))
+      .appendTo(additional)
     $("<option>")
       .val("@custom")
       .text(brapi.i18n.getMessage("options_enable_custom_voices"))
