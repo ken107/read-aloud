f610639e9b70d2b710447a6ccf8bfe6931cbf6b4
Hai Phan <hai.phan@gmail.com>
2026-02-05 00:13:31 +0700

    #479 revokeObjectURL after audio playback finishes

--- js/events.js
+++ js/events.js
@@ -10,7 +10,8 @@ const audioPlayer = immediate(() => {
   return {
     play(src, opts) {
       if (current) current.playback.unsubscribe()
-      const url = (src instanceof Blob) ? URL.createObjectURL(src) : src
+      const isBlob = src instanceof Blob
+      const url = isBlob ? URL.createObjectURL(src) : src
       const playbackState$ = new rxjs.BehaviorSubject("resumed")
       return new Promise((fulfill, reject) => {
         current = {
@@ -20,6 +21,7 @@ const audioPlayer = immediate(() => {
             error: reject
           })
         }
+        if (isBlob) current.playback.add(() => URL.revokeObjectURL(url))
       })
     },
     pause() {
